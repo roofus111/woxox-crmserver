cfafe9b737f5e7605ad2fb5d4483f016018923c2
Technical Lead <tech@canbridge.in>
2025-04-10 11:27:56 +0530

    role controller

--- controllers/roleController.js
+++ controllers/roleController.js
@@ -22,4 +22,50 @@ exports.createRole = async (req, res) => {
     res.status(400).json({ message: error.message });
   }
 };
+// Get all roles
+exports.getAllRoles = async (req, res) => {
+  try {
+    const roles = await Role.find();
+    res.status(200).json(roles);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
 
+// Get role by ID
+exports.getRoleById = async (req, res) => {
+  try {
+    const role = await Role.findById(req.params.id);
+    if (!role) {
+      return res.status(404).json({ message: 'Role not found' });
+    }
+    res.status(200).json(role);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+// Update role by ID
+exports.updateRole = async (req, res) => {
+  try {
+    const updatedRole = await Role.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    if (!updatedRole) {
+      return res.status(404).json({ message: 'Role not found' });
+    }
+    res.status(200).json(updatedRole);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+// Delete role by ID
+exports.deleteRole = async (req, res) => {
+  try {
+    const deletedRole = await Role.findByIdAndDelete(req.params.id);
+    if (!deletedRole) {
+      return res.status(404).json({ message: 'Role not found' });
+    }
+    res.status(204).send(); // No content to send back
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
--- routes/roleRoutes.js
+++ routes/roleRoutes.js
@@ -6,4 +6,8 @@ const authenticateUser = require('../middleware/authenticateUser');
 router.use(authenticateUser);
 
 router.post('/createrole', roleController.createRole);
+router.get('/getallroles', roleController.getAllRoles);
+router.get('/getrolebyid/:id', roleController.getRoleById);
+router.put('/updaterole/:id', roleController.updateRole);
+router.delete('/deleterole/:id', roleController.deleteRole);
 module.exports = router;
